8edbb660edef0d1a4834dd418bbd2a2b44c21887
Maximillian Dornseif <md@hudora.de>
2015-08-27 12:22:40 +0200

    Minimal Testcase

---
+++ examples/minimal.py
@@ -0,0 +1,55 @@
+a = 1
+b = 'a'
+c = 1.2
+d = True  # produces invalid js
+# e = (1, 2)
+f = ['a']
+# g = (1, 2)
+h = [1, 2]
+i = {'a': 'b'}
+# j = {'a': 1}
+# k = {'a', 'b'}
+# l = {'a', 1}
+m = list()
+n = dict()
+o = dict(a='b')  # does not work
+p = set()
+q = set('a')
+
+def foo():
+    return 1
+
+# def bar(a, b):
+#     return [a, b]
+
+# def bar(a, b=1):
+#    return [a, b]
+
+# def getval(a):
+#     return 5 - a
+
+def getval2():
+    # "docstring"
+    localvariable2 = 1
+    localvariable = 1
+    localvariable2 = 2
+    return localvariable
+
+# def lam():
+ #    x = 4
+ #    return lambda x: z+x
+
+# z = 1 + 2 + getval2(getval())
+
+class Fooclass(object):
+    "docstring"
+    pass
+    # classvar = 1  # does not work
+#    def method(self):
+#        return 2
+#    def method2(self, a):
+#        return a
+
+
+a = FooClass()
+a.b = 'c'
